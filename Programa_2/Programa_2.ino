ac86bc9d7151cc44b5b52657cd02db910ae14af1
mherranzcruz2017 <36241957+mherranzcruz@users.noreply.github.com>
2018-02-19 16:59:05 +0100

    Afegir Low, High
    Canviar al programa, els low i high per fer la intermitencia

--- Programa_2/Programa_2.ino
+++ Programa_2/Programa_2.ino
@@ -20,7 +20,7 @@ const int led7 = 12;         // donar nom al pin 12 de l’Arduino
 //*****************************SETUP******************************
 
 void setup() {                 //Configura el final de salida
-    pinMode(led0, OUTPUT);     // definir el pin 5 com una sortida
+  pinMode(led0, OUTPUT);     // definir el pin 5 com una sortida
   pinMode(led1, OUTPUT);     // definir el pin 6 com una sortida
   pinMode(led2, OUTPUT);     // definir el pin 7 com una sortida
   pinMode(led3, OUTPUT);     // definir el pin 8 com una sortida
@@ -36,24 +36,24 @@ void setup() {                 //Configura el final de salida
 
 void loop() {                 //Inicia el bucle del programa
   digitalWrite(led0, HIGH);    // posar a 5V el pin 5
-  digitalWrite(led1, HIGH);    // posar a 5V el pin 6
+  digitalWrite(led1, LOW);    // posar a 5V el pin 6
   digitalWrite(led2, HIGH);    // posar a 5V el pin 7
-  digitalWrite(led3, HIGH);    // posar a 5V el pin 8
+  digitalWrite(led3, LOW);    // posar a 5V el pin 8
   digitalWrite(led4, HIGH);    // posar a 5V el pin 9
-  digitalWrite(led5, HIGH);    // posar a 5V el pin 10
+  digitalWrite(led5, LOW);    // posar a 5V el pin 10
   digitalWrite(led6, HIGH);    // posar a 5V el pin 11
-  digitalWrite(led7, HIGH);    // posar a 5V el pin 12
+  digitalWrite(led7, LOW);    // posar a 5V el pin 12
   
   delay(500);                  // es queden leds 500ms encesos
   
   digitalWrite(led0, LOW);     // posar a 0V el pin 5
-  digitalWrite(led1, LOW);     // posar a 0V el pin 6
+  digitalWrite(led1, HIGH);     // posar a 0V el pin 6
   digitalWrite(led2, LOW);     // posar a 0V el pin 7
-  digitalWrite(led3, LOW);     // posar a 0V el pin 8
+  digitalWrite(led3, HIGH);     // posar a 0V el pin 8
   digitalWrite(led4, LOW);     // posar a 0V el pin 9
-  digitalWrite(led5, LOW);     // posar a 0V el pin 10
+  digitalWrite(led5, HIGH);     // posar a 0V el pin 10
   digitalWrite(led6, LOW);     // posar a 0V el pin 11
-  digitalWrite(led7, LOW);     // posar a 0V el pin 12
+  digitalWrite(led7, HIGH);     // posar a 0V el pin 12
   
   delay(500);                  // es queden leds 500ms apagats
 
