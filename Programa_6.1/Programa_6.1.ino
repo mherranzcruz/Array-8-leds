edec63e748722efc0dcf1eee191ad0196f386939
mherranzcruz2017 <36241957+mherranzcruz@users.noreply.github.com>
2018-03-05 16:26:14 +0100

    Encesa 3 vegades un led amb for
    Encesa 3 vegades un led amb for

---
+++ Programa_6.1/Programa_6.1.ino
@@ -0,0 +1,35 @@
+/*****************************************************************
+**                                                              **
+**                        Títol:                                **
+**           Encesa de 3 leds amb i sense for                   **
+**                                                              **
+** NOM:Martin Herranz Cruz                     Data:05/03/2018  **
+*****************************************************************/
+//****************************INCLUDE*****************************
+
+
+//***************************VARIABLES****************************
+const int led=9;
+signed long retard=500;
+//*****************************SETUP******************************
+
+void setup() {                 //Configura el final de salida
+  for(int i=0; i<3; i++) 
+    {
+  
+pinMode(led,OUTPUT);
+digitalWrite(led,HIGH);
+delay(retard);
+digitalWrite(led,LOW);
+delay(retard);
+    }
+}
+
+//******************************LOOP******************************
+
+void loop() {                 //Inicia el bucle del programa
+
+
+}
+
+//****************************FUNCIONS****************************
